de62f44b8f2cd7f816bf82cd9f500c9a318bcca7
sofyaol <sofyaol@hotmail.com>
2024-03-25 19:18:49 +0500

    Initial Commit

---
+++ vk-infection-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vk-infection-app
+//
+//  Created by Sofya Olekhnovich on 25.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
